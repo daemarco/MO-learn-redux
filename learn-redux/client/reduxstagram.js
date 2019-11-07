386c2e484b202d0b18bb522d1921540fb3feff3e
daemarco <daemarco8in@gmail.com>
2019-11-07 18:18:16 +0100

    MO-learn-redux til lesson 6

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -6,5 +6,19 @@ import css from "./styles/style.styl";
 
 // import components
 import Main from "./components/main";
+import Single from "./components/single";
+import PhotoGrid from "./components/photo-grid";
 
-render(<Main />, document.getElementById("root"));
+// import react router depts
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById("root"));
